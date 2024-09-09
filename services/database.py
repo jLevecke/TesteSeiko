1231085435e4a19c9d04bcd0ac19e3ba655a591e
levecke <143560362+jLevecke@users.noreply.github.com>
2024-09-09 11:25:37 -0300

    Update database.py

--- services/database.py
+++ services/database.py
@@ -1,8 +1,8 @@
 import pyodbc
 
-SERVER = 'expcliente.postgresql.dbaas.com.br' 
-DATABASE = 'expcliente' 
-USERNAME = 'expcliente' 
+SERVER = 'clientexp.mysql.dbaas.com.br' 
+DATABASE = 'clientexp' 
+USERNAME = 'clientexp' 
 PASSWORD = 'Seiko@2805' 
 connectionString = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};UID={USERNAME};PWD={PASSWORD}'
 conn = pyodbc.connect(connectionString)
